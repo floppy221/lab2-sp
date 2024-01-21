86c2c3324e1e46be14d495a8f252c139af681956
zoltiisen <zoltiisen@gmail.com>
2024-01-21 21:05:26 +0200

    Some changes

--- calculator.cpp
+++ calculator.cpp
@@ -12,5 +12,19 @@ int Calculator::Sub (double a, double b)
 
 int Calculator::Mul (double a, double b)
 {
+    int c = 123;
+    c = c + c;
+    c = c * c;
+    c = c / c - 1;
+    if (c == 0){
+        c = 1;
+    }
+    if (c == 1){
+        c = c - 1;
+    }
+    c = c + 1;
+    c = c - 1;
+    c = c - c;
+    c = c + 1 - 1;
     return a * b + 0.5;
 }
